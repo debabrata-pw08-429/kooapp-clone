0a802089e0e2e74465e08ac392b8d577db5ad687
debabrata-pw08-429 <chintuthecoder@gmail.com>
2023-03-15 01:37:04 +0530

    Built custom database json server

--- kooapp/src/Components/Navbar.jsx
+++ kooapp/src/Components/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import data from "../database/db.json";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      {data.peopleData.map((ele) => {
+        if (ele.name === "Virat Kohli")
+          return (
+            <div key={ele.id} style={{ width: "55%", margin: "50px auto" }}>
+              <img
+                src={ele.posts[0].content.imgContent}
+                style={{ width: "50%" }}
+              ></img>
+              ;
+            </div>
+          );
+      })}
+    </div>
+  );
 };
 
 export default Navbar;
