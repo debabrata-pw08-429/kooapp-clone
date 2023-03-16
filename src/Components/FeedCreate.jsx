80ee5c17bdf5c281572a6760e7aa41493946b8e3
syed2605 <gousesyed97@gmail.com>
2023-03-16 20:18:12 +0530

    FeedCreate

---
+++ src/Components/FeedCreate.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {Text,Avatar,Stack,InputGroup,InputLeftElement,Input,InputRightElement,Flex,Box,} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { useSelector } from "react-redux";
+
+function FeedCreate() {
+    let { Image } = useSelector((state) => state.LoggedReducer);
+  return (
+    <div>
+        <Link to="/create">
+            <Stack padding="10px" spacing={4} >
+              <InputGroup pointer="cursor" style={{backgroundColor:'#ffffff'}}>
+                <InputLeftElement
+                  children={
+                    <div>
+                      <Avatar src={Image} size="xs" ml={4} mr={2} />
+                    </div>
+                  }
+                />
+                <Input type="text" placeholder="What's on your mind?" color={'#CCCCCC'} fontSize='14px' />
+                <InputRightElement
+                  children={
+                    <AiFillPlusCircle size={19} style={{ fill: "#474747" }} />
+                  }
+                />
+              </InputGroup>
+            </Stack>
+          </Link>
+    </div>
+  )
+}
+
+export default FeedCreate
