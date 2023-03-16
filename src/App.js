a19445180e8c66db73d665381fee5e782b6f2e7a
Keerthimalini6 <keerthimalini6699@gmail.com>
2023-03-16 22:15:30 +0530

    started bio page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import People from "./Pages/People";
-// import { Login } from "./Components/Login";
+import {Routes,Route} from 'react-router-dom'
+import { Bio } from "./Components/Bio";
 
 function App() {
   return (
     <div className="App">
-      <ChakraProvider>
-        <People />
-        {/* <Login /> */}
-      </ChakraProvider>
+      <Routes> 
+      <Route path="/" element={<People />}  ></Route>      
+      <Route path="/bio" element={<Bio/>}  ></Route>    
+      </Routes>
+      
     </div>
   );
 }
