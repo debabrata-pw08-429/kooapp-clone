da37281dfc5f1623347994737b47b15722991843
debabrata-pw08-429 <chintuthecoder@gmail.com>
2023-03-16 19:37:01 +0530

    Navbar Updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import People from "./Pages/People";
-import Rsidebar from "./Components/Rsidebar";
+// import People from "./Pages/People";
+// import Rsidebar from "./Components/Rsidebar";
 // import { Login } from "./Components/Login";
 // import Home from "./Pages/Home";
-
+import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div className="App">
       <ChakraProvider>
         {/* <People /> */}
         {/* <Login /> */}
-      <Rsidebar/>
+        {/* <Rsidebar /> */}
+        <Navbar />
       </ChakraProvider>
     </div>
   );
