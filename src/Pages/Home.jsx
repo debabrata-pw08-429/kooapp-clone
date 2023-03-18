26ad48a48bf63a4a8606d51551cc8f5af4d154a8
debabrata-pw08-429 <chintuthecoder@gmail.com>
2023-03-18 16:43:43 +0530

    Homepage Done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,76 +1,68 @@
 import "../Styles/stats.css";
 import Navbar from "../Components/Navbar";
-import {
-  Box,
-  Heading,
-  HStack,
-  Stack,
-  Text,
-  Spacer,
-  Button,
-  Divider,
-  Flex,
-} from "@chakra-ui/react";
-// import { Image } from "@chakra-ui/react";
-// import { BsArrowRight } from "react-icons/bs";
-// import { theme } from "../Styles/theme/brandTheme";
-
-// import Koo_tagline_ss from "../Images/Koo_tagline_ss.png";
-// import mapbackground from "../Images/mapbackground.webp";
-import Section3 from "../Components/Section3";
-import Section2 from "../Components/Section2";
 import Section1 from "../Components/Section1";
+import Section2 from "../Components/Section2";
+import Section3 from "../Components/Section3";
+import Section4 from "../Components/Section4";
+import Footer from "../Components/Footer";
+import { HStack, Stack, VStack } from "@chakra-ui/react";
 
 const Home = () => {
+  const links = {
+    quickLinks: [
+      { text: "About Us", href: "https://info.kooapp.com/category/about-us/" },
+      {
+        text: "In the Press",
+        href: "https://medium.com/koo-app/koo-media-coverage-389c1a174026",
+      },
+      { text: "FAQs", href: "https://info.kooapp.com/faq/" },
+      {
+        text: "Eminence",
+        href: "https://info.kooapp.com/koo-accounts-of-eminence/",
+      },
+      { text: "Download App", href: "https://app.adjust.net.in/zf169cq" },
+    ],
+    supportLinks: [
+      { text: "Contact Us", href: "https://info.kooapp.com/contact-us/" },
+      {
+        text: "Privacy Policy",
+        href: "https://info.kooapp.com/privacy-policy/",
+      },
+      {
+        text: "Terms of Service",
+        href: "https://info.kooapp.com/terms-of-service/",
+      },
+      {
+        text: "Community Guidelines",
+        href: "https://info.kooapp.com/community-guidelines/",
+      },
+      {
+        text: "Compliance",
+        href: "https://info.kooapp.com/category/compliance/",
+      },
+    ],
+  };
+
   return (
     <div>
       <Navbar />
       <Stack>
         <HStack>
           <Section1 />
-          {/* <Box width={["614px"]}>
-            <Box width={["250px"]} height="100%">
-              <Heading>Know what's happening!</Heading>
-              <Text>All in your Language</Text>
-              <Button
-                boxSize="100%"
-                borderRadius="full"
-                bg={theme.brand.colors.btn_bg_login}
-                color="white"
-                py="1rem"
-              >
-                Go to Feed
-                <BsArrowRight pl="1rem" />
-              </Button>
-            </Box>
-          </Box>
-          <Spacer />
-          <Box width={["1090px"]}>
-            <Image src="https://images.kooapp.com/img/home-new.png" />
-          </Box> */}
         </HStack>
-        <HStack>
+        <HStack width={window.innerWidth}>
           <Section2 />
         </HStack>
         <HStack>
           <Section3 />
         </HStack>
+        <HStack>
+          <Section4 />
+        </HStack>
+        <VStack>
+          <Footer links={links} />
+        </VStack>
       </Stack>
-
-      {/* <Grid
-        // templateAreas={[
-        //   `"1"
-        // "2"`,
-        //   `"1 2"`,
-        // ]}
-        gridTemplateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]}
-        height="600px"
-        gap={6}
-        bg="#f1f1ee"
-      >
-        <GridItem position="relative" top={["420px", "0px"]}></GridItem>
-        <GridItem position="relative" bottom={["150px", "0px"]}></GridItem>
-      </Grid> */}
     </div>
   );
 };
